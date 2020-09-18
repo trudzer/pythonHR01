695f788af32d21c569d6c1aad3edc85b5b6a1ddb
trudzer <trudzeredits@gmail.com>
2020-09-18 12:24:09 +0000

    Add files via upload

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,24 +1,6 @@
 #Úlfur Ingólfsson 18/09/2020
-def walk(direction):
-  pos1 = 0
-  pos2 = 0
-
-  if direction == "e":
-    pos1 = 1
-  
-  if direction == "n":
-    pos2 = 1
-
-  if direction == "w":
-    pos1 = -1
-
-  if direction == "s":
-    pos2 = -1
-  
-  return pos1,pos2
 post1 = 1
 post2 = 1
-wall = False
 print("You can travel: (N)orth.")
 inp = input("choose direction: ").lower()
 if inp != "n":
@@ -26,7 +8,6 @@ if inp != "n":
 else:
   post2 += 1
 if post1 >= 1 and post2 >= 1 or post1 <= 3 and post2 <= 3:
-  direct_inp = list (walk(inp))
 
   if post2 > 3:
     post2 = 3
@@ -96,8 +77,6 @@ while True:
     elif inp == "s":
       post2 -= 1
        
-  direct_inp[0] = 0
-  direct_inp[1] = 0
   if post1 == 3 and post2 == 1:
     print("Victory!")
     break
