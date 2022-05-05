94a1720d084cc4b02bdd60fa64211e71d9ea48be
trudzer <trudzeredits@gmail.com>
2022-05-05 19:32:07 +0000

    Update basic_sorting.py

--- basic_sorting.py
+++ basic_sorting.py
@@ -1,16 +1,19 @@
 ARRAY = [1, 3, 6, 4, 8, 2, 11, 5, 9, 7, 12, 10]
 
+"""Swap 2 elements of an array"""
 def swapElements(a, x, y):
     z = a[x]
     a[x] = a[y]
     a[y] = z
 
+"""Sort array from lowest to highest""" 
 def sortArray(x):
     for i in range(len(x)):
         for j in range(len(x)):
             if (x[j] > x[i]):
                 swapElements(x, i, j)
                 
+"""Display odd numbers first in ascending order, then even numbers in descending order"""
 def oddThenEven(x):
     sortArr(x)
     for i in range(len(x)):
