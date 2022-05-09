f6659d42612e65ba6a970f2b518a9d1ad87796d6
trudzer <trudzeredits@gmail.com>
2022-05-09 18:32:53 +0000

    Create collision_check.py

---
+++ collision_check.py
@@ -0,0 +1,26 @@
+import math
+
+class Player:
+    def __init__(self, height = 0, radius = 0, pos = (0, 0, 0)):
+        self.height = height
+        self.radius = radius
+        self.pos = pos
+
+def vectorAtoB(a, b):
+    return math.sqrt(math.pow((b.pos[0] - a.pos[0]), 2) + math.pow((b.pos[1] - a.pos[1]), 2) + math.pow((b.pos[2] - a.pos[2]), 2))
+
+def checkCollision(a, b):
+    if (a.radius + b.radius) > vectorAtoB(a, b):
+        return True
+    else:
+        return False
+
+def main():
+    P1 = Player(3, 2, (-2,2,0))
+    P2 = Player(4, 4, (5,4,2))
+    print("Combined radius:\t" + str(P1.radius + P2.radius))
+    print("Vector from a to b:\t" + str(round(vectorAtoB(P1, P2), 2)))
+    print("Players touching:\t" + str(checkCollision(P1, P2)))
+    
+if __name__ == "__main__":
+    main()
