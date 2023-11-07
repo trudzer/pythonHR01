c42f527913b3d82b299a85432668d0cef1f06105
trudzer <trudzeredits@gmail.com>
2023-11-07 22:35:06 +0000

    Update BasketHeightCalculator.py

--- BasketHeightCalculator.py
+++ BasketHeightCalculator.py
@@ -5,32 +5,47 @@ def calcReach(height, ws):
 
 def calcHeight(height, ws, basket, HEIGHT, WS):
     differenceInReach = calcReach(height, ws) - calcReach(HEIGHT, WS)
-    equivalent_basket_height = basket - differenceInReach
-    return math.floor(equivalent_basket_height)
+    equivalentBasketHeight = basket - differenceInReach
+    return math.floor(equivalentBasketHeight)
 
-def calcCMtoInch(height_cm):
-    feet = math.floor(height_cm / 30.48)
-    inches = round((height_cm % 30.48) / 2.54)
+def calcCMtoInch(heightCM):
+    feet = math.floor(heightCM / 30.48)
+    inches = round((heightCM % 30.48) / 2.54)
     if inches == 12:
         feet += 1
         inches = 0
     newHeight = "{}'{}\"".format(feet, inches)
     return newHeight
+    
+def calcInchtoCM(heightInch):
+    feet, inches = map(int, heightInch.split("'"))
+    totalCM = feet * 30.48 + inches * 2.54
+    return totalCM
 
 def main():
     HEIGHT = 190
     WS = 194
     BASKET = 305
-
-    otherHeight = int(input("Enter the height of the person (in cm): "))
-    otherWS = int(input("Enter the wingspan of the person (in cm): "))
     REACH = calcReach(HEIGHT, WS)
+
+    otherHeight = input("Enter the height of the person in (cm) or (ft): ")
+    otherWS = input("Enter the wingspan of the person in (cm) or (ft): ")
+    
+    if "'" in otherHeight:
+        otherHeight = int(calcInchtoCM(otherHeight))
+    else:
+        otherHeight = int(otherHeight)
+    if "'" in otherWS:
+        otherWS = int(calcInchtoCM(otherWS))
+    else:
+        otherWS = int(otherWS)
+        
     otherReach = calcReach(otherHeight, otherWS)
     equivalentHeight = calcHeight(otherHeight, otherWS, BASKET, HEIGHT, WS)
 
-    print(f"\nThe person with height {HEIGHT}cm ({calcCMtoInch(HEIGHT)}), wingspan {WS}cm ({calcCMtoInch(WS)}) and standing reach {REACH}cm ({calcCMtoInch(REACH)})")
-    print(f"The person with height {otherHeight}cm ({calcCMtoInch(otherHeight)}), wingspan {otherWS}cm ({calcCMtoInch(otherWS)}) and standing reach {otherReach}cm ({calcCMtoInch(otherReach)})")
-    print(f"A basket height of {equivalentHeight}cm ({calcCMtoInch(equivalentHeight)}) is equivalent to a basket height of {BASKET}cm ({calcCMtoInch(BASKET)})")
+    print(f"\n(A) Height: {HEIGHT}cm ({calcCMtoInch(HEIGHT)}), wingspan: {WS}cm ({calcCMtoInch(WS)}) and standing reach: {REACH}cm ({calcCMtoInch(REACH)})")
+    print(f"(B) Height: {otherHeight}cm ({calcCMtoInch(otherHeight)}), wingspan: {otherWS}cm ({calcCMtoInch(otherWS)}) and standing reach: {otherReach}cm ({calcCMtoInch(otherReach)})")
+    print(f"A basket height of {equivalentHeight}cm ({calcCMtoInch(equivalentHeight)}) for person (A) is equivalent to a basket height of {BASKET}cm ({calcCMtoInch(BASKET)}) for person (B)")
 
 if __name__ == "__main__":
     main()
